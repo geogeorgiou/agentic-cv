e3e6abee9fb6a285b1497088d2edb4bc4525ecc0
George Georgiou <geogeorgiou@192.168.1.10>
2026-04-10 21:11:14 +0300

    adds deploy to GH workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/agentic-cv/',
   plugins: [tailwindcss(), react()],
   resolve: {
     tsconfigPaths: true,
